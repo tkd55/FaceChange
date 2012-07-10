ffbcba06b736529a86ddc21b9451df963f08a76a
tkd55 <tomomichi.takeda@gmail.com>
2012-07-10 11:40:52 +0900

    コメントアウト部分の削除

--- main.cpp
+++ main.cpp
@@ -5,10 +5,9 @@
 //========================================================================
 int main( ){
 
-//	ofSetupOpenGL(1280, 960, OF_WINDOW);			// <-------- setup the GL context
+    //GL context
     ofAppGlutWindow window;
     ofSetupOpenGL(&window, 1280, 960, OF_WINDOW);
-//	ofSetupOpenGL(&window, 640, 480, OF_WINDOW);
     
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
